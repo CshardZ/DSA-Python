bf90ccaf85d61bcb02baf65a97bf3d118d71c27f
VIvek <108602472+CshardZ@users.noreply.github.com>
2023-07-13 20:11:23 +0530

    Update TSP-Recursive-Dynamic.py
    TSP Optimized solution.

--- Algorithms/Graph-Algorithms/Travelling-Salesman-Problem/TSP-Recursive-Dynamic.py
+++ Algorithms/Graph-Algorithms/Travelling-Salesman-Problem/TSP-Recursive-Dynamic.py
@@ -1,6 +1,6 @@
 # Working solution   - YES
-# Solution Level     - Sub-Optimal
-# Helping comments   - NO
+# Solution Level     - Optimal(Can be refractored,there are so many variables used.)
+# Helping comments   - YES
 
 def recur_sol(cities, costs, start, enter, visited):
     # Base case
@@ -10,34 +10,72 @@ def recur_sol(cities, costs, start, enter, visited):
     # Startup Procedure
     each_cost = 0
     all_cost = []
-    city_copy = cities.copy()
+    
+    # Duplicating the cities list, to avoid memory overlap in lists.
+    city_copy = cities[:]
     city_copy.remove(start)
+    
     path = []
     all_path = []
-    res_path = [start]
+    result_path = [start]
     
     # Main logic
+    # Scroll down to get help on <Tags>.
     for city in city_copy:
         path = []
+        next_city = []
+        
+        #Tag-1
         if not visited[city]:
             each_cost = 0
             path.extend([city])
-            each_cost,next_city = recur_sol(city_copy,costs, city,enter,[False]*len(visited)) 
-            path.extend(next_city)
             
-        each_cost += costs[start][city]
-        all_path.append(path)
-        all_cost.append(each_cost)
-        
+            # Recursive call
+            each_cost,next_city = recur_sol(city_copy,costs, city,enter,[False]*len(visited)) 
+            
+            # Tag-2
+            each_cost += costs[start][city]
+            all_cost.append(each_cost)
+            
+            path.extend(next_city)
+            all_path.append(path)
+
+    # Tag-3 
+    result_path.extend(all_path[all_cost.index(min(all_cost))])  
+
+    # Tag-4
+    if result_path[0] != enter:
+        result_path.pop(0)
+    else:
+        result_path.append(enter)
+    return min(all_cost),result_path
+
+"""
+Helping Documentation:
+
+#Tag-1:
+    The function is designed in such a way that the the <Tag-1> if condition will not get False.(Nothing special)
+    Thus indenting the code blocks after <Tag-3> till return(excluding) under <Tag-3> if does not matter.
+    It will  get executed regardless of <Tag-1> if condition.
+
+#Tag-2 :
+    The of each path from start to next_city calculated recursive manner. It is writtern after recursive call so that sub_paths+current_path can be done.
+    Else the current_path cost will get erased on every recursive call.
+
+#Tag-3 : 
+    ```
     final_cost = min(all_cost)
     idx = all_cost.index(final_cost)
     final_path = all_path[idx]
-
     res_path.extend(final_path)
-    if res_path[0] != enter:
-        res_path.pop(0)
-    else:
-        res_path.append(enter)
+    ```
+    
+         Explaination: 
+         The path and its cost is stored in corresponding position of all_path and all_cost.
+         result_path already contains  starting city. [1] if start=1
+         Extend the result_path with the path which has minimum of all_cost.
 
-    #print(all_path,'   ',res_path)
-    return final_cost,res_path   
+#Tag-4 : 
+     This is just an adjustment code, else directly returning result path would lead to something like this = [1,2,2,3,3,4] etc
+     To avoid this I've used the  code.
+"""
